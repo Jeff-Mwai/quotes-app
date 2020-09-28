9fae4808e81a9d23536ca4b42f977c0339066ece
Jeff-Mwai <jeffmwai3@gmail.com>
2020-09-28 10:52:47 +0300

    Edited the quote details component

--- src/app/component/cards/cards.component.ts
+++ src/app/component/cards/cards.component.ts
@@ -8,14 +8,16 @@ import { Quote } from 'src/app/quote';
 })
 export class CardsComponent implements OnInit {
   quotes = [
+    new Quote('Paul', 'Elon Musk', 'Its OK to have your eggs in one basket as long as you control what happens to that basket.', new Date(2020, 12, 4)),
+    new Quote('Jeff', 'Bob Marley', 'Do not worry about a thing, cause every little thing gonna be all right', new Date(2020, 9, 2)),
     new Quote('Kennedy', 'Muhammad Ali', 'The fight is won or lost far away from witnesses - behind the lines, in the gym, and out there on the road, long before I dance under those lights', new Date(2020, 9, 2)),
-    new Quote('Francis', 'Alexander Bell', 'Your time is limited, so don nott waste it living someone elses life. Do not be trapped by dogma – which is living with the results of other peoples thinking', new Date(2020, 9, 26)),
-    new Quote('Jeff', 'Bob Marley','Do not worry about a thing, cause every little thing gonna be all right', new Date(2020, 9, 2)),
+    new Quote('Francis', 'Steve Jobs', 'Your time is limited, so don not waste it living someones life. Do not be trapped by dogma – which is living with the results of other peoples thinking', new Date(2020, 9, 26))
+    
 
   ]
-num1: number;
-num2: number;
-i: number;
+  num1: number;
+  num2: number;
+  i: number;
 
   constructor() { }
 
@@ -33,14 +35,14 @@ i: number;
   addQuote(newQoute) {
     this.quotes.push(newQoute);
   }
- /*  highlight() {
-    this.num1 = 0;
-    this.num2 = 0;
-    for(this.i=0; this.i<this.quotes.length; this.i++){
-    this.num2 = this.quotes[this.i].upvote;
-    if (this.num2 > this.num1) {
-    this.num1=this.num2;
-    }return this.num1
-    }
-  } */
+  /*  highlight() {
+     this.num1 = 0;
+     this.num2 = 0;
+     for(this.i=0; this.i<this.quotes.length; this.i++){
+     this.num2 = this.quotes[this.i].upvote;
+     if (this.num2 > this.num1) {
+     this.num1=this.num2;
+     }return this.num1
+     }
+   } */
 }
